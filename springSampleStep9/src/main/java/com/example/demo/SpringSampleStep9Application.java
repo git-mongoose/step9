6222328d1848aa705bd894143f195402805ba342
hama <hama@192.168.100.115>
2022-12-23 14:15:46 +0900

    環境構築

---
+++ springSampleStep9/src/main/java/com/example/demo/SpringSampleStep9Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleStep9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleStep9Application.class, args);
+	}
+
+}
